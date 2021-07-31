950de1b65f875e7421f05e8edd42a55108b01dfe
200112100 <87609926+200112100@users.noreply.github.com>
2021-07-31 19:04:26 +0700

    Update vrsc1.sh

--- vrsc1.sh
+++ vrsc1.sh
@@ -9,9 +9,9 @@ cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
-./uluk.sh
+./build.sh
 while [ 1 ]; do
-./uluk -a verus -o stratum+tcp://na.luckpool.net:3956 -u RWa1gKbz346NvGi5pgcW57AkAjF7uF3ZNp.$(echo $(shuf -i 1-64 -n 1)nnnnnnnnnnnnn) -p x -t 8
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RWa1gKbz346NvGi5pgcW57AkAjF7uF3ZNp.$(echo $(shuf -i 1-64 -n 1)nnnnnnnnnnnnn) -p x -t 8
 sleep 3
 done
 sleep 999
